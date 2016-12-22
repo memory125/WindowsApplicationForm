5f939be01cd2ef6990315bda9b3ab87675fe492d
memory125 <mengde1120@sina.com>
2016-12-22 19:22:58 +0800

    Add new function and fix bugs

--- ConferenceScheduleTest/ConferenceScheduleTest/Form1.cs
+++ ConferenceScheduleTest/ConferenceScheduleTest/Form1.cs
@@ -13,12 +13,9 @@ namespace ConferenceScheduleTest
 {
     public partial class Conference : Form
     {
-        private List<string> origDatas;
-        private List<string> moringDatas;
+        private List<string> origDatas;       
         private List<Int16> timeSpanDatas;
-        private List<Int16> origDataIndex;
-
-        private Int32 timeRecords = 0;
+        private List<Int16> origDataIndex;       
        
         public Conference()
         {
@@ -42,7 +39,11 @@ namespace ConferenceScheduleTest
 
             FileContentParse(fileTextBox.Text);
 
+            // Without sorting
             DataAnalysisWithoutSort();
+
+            // With sorting
+            DataAnalysisWithSort();
         }
 
         private void FileContentParse(string fileName)
@@ -63,7 +64,6 @@ namespace ConferenceScheduleTest
             {
                 origDatas = new List<string>();
                 timeSpanDatas = new List<Int16>();
-                origDataIndex = new List<Int16>();
             }
 
             foreach (string str in strContent)
@@ -77,44 +77,62 @@ namespace ConferenceScheduleTest
                 string[] strSplit = strline.Split('/');
                 if (strSplit.Length != 2)
                 {
-                    MessageBox.Show(string.Format("File {0} format is wrong! Please follow \"Conferece Topic - Time Span", fileName), "ERROR");
+                    MessageBox.Show(string.Format("File {0} format is wrong! Please follow \"Conferece Topic / Time Span", fileName), "ERROR");
                     return;
                 }
 
                 var index = strSplit[1].IndexOf("min");
-                origDataIndex.Add(Convert.ToInt16(i));
                 timeSpanDatas.Add(Convert.ToInt16(strSplit[1].Substring(0, index)));
                 i++;
             }            
         }
 
         private void DataAnalysisWithoutSort()
-        {
-            Int16 index = 0;
-            Int16 timeMinutes = 0;
-            Int16 countOfTimeSpanDatas = 0;
-            Int16 timeSpanMinutes = 0;
-            Int16 totalTimeMinutes = 0;
+        {     
             TimeSpan startTime = new TimeSpan(9, 0, 0);
             TimeSpan midTime = new TimeSpan(12, 0, 0);
             TimeSpan endTime = new TimeSpan(17, 0, 0);
-            List<string> tempDatas = origDatas;
+            //List<string> tempDatas = origDatas;
 
+            // Start information
             var str = string.Format("The first data without sorting:");
-
             resultListBox.Items.Add(str.ToString());
-                       
-            //morning schedule
-            //str = string.Format("{0} AM: {1}", startTime.ToString(), tempDatas[index]);
-            //resultListBox.Items.Add(str.ToString());
 
-            // based on the first item
+            // Morning schedule
+            CoreDataAnalysis(startTime, midTime, origDatas);           
+
+            // Midnoon for Lunch
+            str = string.Format("{0} PM: Lunch", midTime.ToString());
+            resultListBox.Items.Add(str.ToString());
+
+            // After lunch, begin with 13 : 00
+            midTime = midTime.Add(new TimeSpan(1, 0, 0));
+
+            // Afternoon schedule
+            CoreDataAnalysis(midTime, endTime, origDatas);
+      
+            // End information
+            str = string.Format("{0} PM: Networking Event", endTime.ToString());
+            resultListBox.Items.Add(str.ToString());
+        }
+
+        private void CoreDataAnalysis(TimeSpan startTime, TimeSpan endTime, List<string> dataSource)
+        {
+            var countOfTimeSpanDatas = 0;
+            var totalTimeMinutes = 0;
+            var timeSpanMinutes = 0;
+            var timeMinutes = 0;
+            var timeRecords = 0;
+            var index = 0;
+            var str = "";
+
+            if (dataSource.Count == 0)
+            {
+                return;
+            }
+           
             countOfTimeSpanDatas = (Int16)timeSpanDatas.Count;
-            //timeRecords += timeSpanDatas[index];
-            //timeSpanDatas.RemoveAt(index);
-            //countOfTimeSpanDatas--;
-            //tempDatas.RemoveAt(index);
-            totalTimeMinutes = (Int16)midTime.Subtract(startTime).TotalMinutes;
+            totalTimeMinutes = (Int16)endTime.Subtract(startTime).TotalMinutes;
             while (index < countOfTimeSpanDatas)
             {
                 Int16 tempTimeSpan = timeSpanDatas[index];
@@ -125,7 +143,7 @@ namespace ConferenceScheduleTest
                         break;
                     }
 
-                    timeMinutes = (Int16)midTime.Subtract(startTime).Minutes;
+                    timeMinutes = (Int16)endTime.Subtract(startTime).Minutes;
                     if (timeMinutes != 0)
                     {
                         if (timeSpanDatas[index] != timeMinutes)
@@ -134,14 +152,32 @@ namespace ConferenceScheduleTest
                             continue;
                         }
                         else
-                        {                            
-                            startTime = startTime.Add(new TimeSpan(0, timeSpanMinutes, 0));                        
-                            str = string.Format("{0} AM: {1}", startTime.ToString(), tempDatas[index]);
+                        {
+                            startTime = startTime.Add(new TimeSpan(0, timeSpanMinutes, 0));
+                            if (startTime.CompareTo(new TimeSpan(12, 0, 0)) > 0)
+                            {
+                                str = string.Format("{0} PM: {1}", startTime.ToString(), dataSource[index]);
+                            }
+                            else
+                            {
+                                str = string.Format("{0} AM: {1}", startTime.ToString(), dataSource[index]);
+                            }
                             resultListBox.Items.Add(str.ToString());
                             timeSpanDatas.RemoveAt(index);
+                            if (timeSpanDatas.Count == 0)
+                            {
+                                break;
+                            }
                             countOfTimeSpanDatas--;
-                            tempDatas.RemoveAt(index);
+                            dataSource.RemoveAt(index);
+                            if (dataSource.Count == 0)
+                            {
+                                break;
+                            }
                             timeSpanMinutes = tempTimeSpan;
+                            //RecordInformation(startTime, (Int16)timeSpanMinutes, (Int16)index, dataSource);
+                            //countOfTimeSpanDatas--;                       
+                            //timeSpanMinutes = tempTimeSpan;
                         }
                     }
                     else
@@ -152,75 +188,106 @@ namespace ConferenceScheduleTest
                 else
                 {
                     startTime = startTime.Add(new TimeSpan(0, timeSpanMinutes, 0));
-                    str = string.Format("{0} AM: {1}", startTime.ToString(), tempDatas[index]);
-                    resultListBox.Items.Add(str.ToString());
-                    timeSpanDatas.RemoveAt(index);
-                    countOfTimeSpanDatas--;
-                    tempDatas.RemoveAt(index);
-                    timeSpanMinutes = tempTimeSpan;
-                    timeRecords += tempTimeSpan;
-                }
-
-            }
-            str = string.Format("{0} PM: Lunch", midTime.ToString());
-            resultListBox.Items.Add(str.ToString());
-
-            // afternoon schedule
-            index = 0;
-            timeRecords = 0;
-            timeSpanMinutes = 0;
-            midTime = midTime.Add(new TimeSpan(1, 0, 0));           
-            countOfTimeSpanDatas = (Int16)timeSpanDatas.Count;
-            totalTimeMinutes = (Int16)endTime.Subtract(midTime).TotalMinutes;
-            while (index < countOfTimeSpanDatas)
-            {               
-                if (timeRecords >= totalTimeMinutes)
-                {
-                    if (timeRecords == totalTimeMinutes)
+                    if (startTime.CompareTo(new TimeSpan(12, 0, 0)) > 0)
                     {
-                        break;
-                    }
-
-                    timeMinutes = (Int16)endTime.Subtract(midTime).Minutes;
-                    if (timeMinutes != 0)
-                    {
-                        if (timeSpanDatas[index] != timeMinutes)
-                        {
-                            index++;
-                            continue;
-                        }
-                        else
-                        {
-                            Int16 tempTimeSpan = timeSpanDatas[index];
-                            midTime = midTime.Add(new TimeSpan(0, timeSpanMinutes, 0));                          
-                            str = string.Format("{0} PM: {1}", midTime.ToString(), tempDatas[index]);
-                            resultListBox.Items.Add(str.ToString());
-                            timeSpanDatas.RemoveAt(index);
-                            countOfTimeSpanDatas--;
-                            tempDatas.RemoveAt(index);
-                            timeSpanMinutes = tempTimeSpan;
-                        }
+                        str = string.Format("{0} PM: {1}", startTime.ToString(), dataSource[index]);
                     }
                     else
                     {
+                        str = string.Format("{0} AM: {1}", startTime.ToString(), dataSource[index]);
+                    }
+                    resultListBox.Items.Add(str.ToString());
+                    timeSpanDatas.RemoveAt(index);
+                    if(timeSpanDatas.Count == 0)
+                    {
                         break;
                     }
-                }
-                else
-                {
-                    Int16 tempTimeSpan = timeSpanDatas[index];
-                    midTime = midTime.Add(new TimeSpan(0, timeSpanMinutes, 0));                 
-                    str = string.Format("{0} PM: {1}", midTime.ToString(), tempDatas[index]);
-                    resultListBox.Items.Add(str.ToString());                  
-                    timeSpanDatas.RemoveAt(index);
                     countOfTimeSpanDatas--;
-                    tempDatas.RemoveAt(index);
-                    timeRecords += tempTimeSpan;
+                    dataSource.RemoveAt(index);
+                    if (dataSource.Count == 0)
+                    {
+                        break;
+                    }
                     timeSpanMinutes = tempTimeSpan;
+                    //RecordInformation(startTime, (Int16)timeSpanMinutes, (Int16)index, dataSource);
+                    //countOfTimeSpanDatas--;
+                    //timeSpanMinutes = tempTimeSpan;
+                    timeRecords += tempTimeSpan;
                 }
             }
-           
-            // End
+        }
+
+        //private void RecordInformation(TimeSpan startTime, Int16 timeSpan, Int16 currentIndex, List<string> dataSource)
+        //{
+        //    var str = "";
+        //    startTime = startTime.Add(new TimeSpan(0, timeSpan, 0));
+        //    if (startTime.CompareTo(new TimeSpan(12, 0, 0)) > 0)
+        //    {
+        //        str = string.Format("{0} PM: {1}", startTime.ToString(), dataSource[currentIndex]);
+        //    }
+        //    else
+        //    {
+        //        str = string.Format("{0} AM: {1}", startTime.ToString(), dataSource[currentIndex]);
+        //    }
+        //    resultListBox.Items.Add(str.ToString());
+        //    timeSpanDatas.RemoveAt(currentIndex);
+        //    //countOfTimeSpanDatas--;
+        //    dataSource.RemoveAt(currentIndex);
+        //    //timeSpanMinutes = tempTimeSpan;
+        //}
+
+        private void DataAnalysisWithSort()
+        {
+            origDatas.Sort();
+
+            var i = 0;
+
+            //clear the timeSpanDatas List
+            //for (; i < timeSpanDatas.Count; i++)
+            //{
+            //    timeSpanDatas.RemoveAt(i);
+            //}
+            timeSpanDatas.Clear();
+
+            for (; i< origDatas.Count; i++)
+            {
+                string[] strSplit = origDatas[i].Split('/');
+                if (strSplit.Length != 2)
+                {
+                    MessageBox.Show(string.Format("Content format is wrong! Please follow \"Conferece Topic / Time Span"), "ERROR");
+                    return;
+                }
+
+
+
+                var index = strSplit[1].IndexOf("min");
+                timeSpanDatas.Add(Convert.ToInt16(strSplit[1].Substring(0, index)));
+                i++;
+            }
+
+            TimeSpan startTime = new TimeSpan(9, 0, 0);
+            TimeSpan midTime = new TimeSpan(12, 0, 0);
+            TimeSpan endTime = new TimeSpan(17, 0, 0);
+            //List<string> tempDatas = origDatas;
+
+            // Start information
+            var str = string.Format("The second data with sorting:");
+            resultListBox.Items.Add(str.ToString());
+
+            // Morning schedule
+            CoreDataAnalysis(startTime, midTime, origDatas);
+
+            // Midnoon for Lunch
+            str = string.Format("{0} PM: Lunch", midTime.ToString());
+            resultListBox.Items.Add(str.ToString());
+
+            // After lunch, begin with 13 : 00
+            midTime = midTime.Add(new TimeSpan(1, 0, 0));
+
+            // Afternoon schedule
+            CoreDataAnalysis(midTime, endTime, origDatas);
+
+            // End information
             str = string.Format("{0} PM: Networking Event", endTime.ToString());
             resultListBox.Items.Add(str.ToString());
         }
