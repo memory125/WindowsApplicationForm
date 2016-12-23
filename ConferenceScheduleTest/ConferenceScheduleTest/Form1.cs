cb5a3425b26d8324e8e067227d66a55174f040d9
memory125 <mengde1120@sina.com>
2016-12-23 10:47:48 +0800

    Add new function and fix bugs

--- ConferenceScheduleTest/ConferenceScheduleTest/Form1.cs
+++ ConferenceScheduleTest/ConferenceScheduleTest/Form1.cs
@@ -14,8 +14,7 @@ namespace ConferenceScheduleTest
     public partial class Conference : Form
     {
         private List<string> origDatas;       
-        private List<Int16> timeSpanDatas;
-        private List<Int16> origDataIndex;       
+        private List<Int16> timeSpanDatas;    
        
         public Conference()
         {
@@ -24,11 +23,13 @@ namespace ConferenceScheduleTest
 
         private void fileButton_Click(object sender, EventArgs e)
         {
+            //
+            // Open file dialog and choose the file you want to load
+            //
             OpenFileDialog openFileDialog = new OpenFileDialog()
-            {
-                //InitialDirectory = @"C:\",
+            {                
                 RestoreDirectory = true,
-                Filter = "Text Files(*.txt)|*.txt|All Files(*.*)|*.*",       //设置打开文件类型
+                Filter = "Text Files(*.txt)|*.txt|All Files(*.*)|*.*",       
                 FilterIndex = 1,                
             };
             
@@ -37,23 +38,42 @@ namespace ConferenceScheduleTest
                 fileTextBox.Text = openFileDialog.FileName;
             }
 
+            //
+            // Parse the file
+            //
             FileContentParse(fileTextBox.Text);
 
+            //
             // Without sorting
+            //
             DataAnalysisWithoutSort();
 
+            //
             // With sorting
+            //
             DataAnalysisWithSort();
         }
 
+        /**********************************************************
+         * Name: FileContentParse
+         * Function: Parse the loaded file datas         
+         * Parameters： None
+         * Return: void         
+         **********************************************************/
         private void FileContentParse(string fileName)
         {
+            //
+            // Check file if exist
+            //
             if (fileName.Equals("") || fileName.Equals(null))
             {
                 MessageBox.Show(string.Format("File {0} is not exist!", fileName), "ERROR");
                 return;
             }
 
+            //
+            // Read the file datas and check if the file is empty
+            //
             string [] strContent = File.ReadAllLines(fileName);
             if (strContent.Length == 0)
             {
@@ -66,11 +86,17 @@ namespace ConferenceScheduleTest
                 timeSpanDatas = new List<Int16>();
             }
 
+            //
+            // Store the file datas
+            //
             foreach (string str in strContent)
             {
                 origDatas.Add(str);
             }
 
+            //
+            // Parse the data and separate them
+            //
             var i = 0;
             foreach (string strline in strContent)
             {                              
@@ -81,41 +107,67 @@ namespace ConferenceScheduleTest
                     return;
                 }
 
+                //
+                // Get the time span for each itme
+                //
                 var index = strSplit[1].IndexOf("min");
                 timeSpanDatas.Add(Convert.ToInt16(strSplit[1].Substring(0, index)));
                 i++;
             }            
         }
 
+        /**********************************************************
+         * Name: DataAnalysisWithoutSort
+         * Function: Test the conference schedule without sorting         
+         * Parameters： None
+         * Return: void         
+         **********************************************************/
         private void DataAnalysisWithoutSort()
         {     
             TimeSpan startTime = new TimeSpan(9, 0, 0);
             TimeSpan midTime = new TimeSpan(12, 0, 0);
             TimeSpan endTime = new TimeSpan(17, 0, 0);
-            //List<string> tempDatas = origDatas;
-
+          
+            //
             // Start information
+            //
             var str = string.Format("The first data without sorting:");
             resultListBox.Items.Add(str.ToString());
 
+            //
             // Morning schedule
+            //
             CoreDataAnalysis(startTime, midTime, origDatas);           
 
-            // Midnoon for Lunch
+            //
+            // Middle noon for Lunch
+            //
             str = string.Format("{0} PM: Lunch", midTime.ToString());
             resultListBox.Items.Add(str.ToString());
 
+            //
             // After lunch, begin with 13 : 00
+            //
             midTime = midTime.Add(new TimeSpan(1, 0, 0));
 
+            //
             // Afternoon schedule
+            //
             CoreDataAnalysis(midTime, endTime, origDatas);
       
+            //
             // End information
+            //
             str = string.Format("{0} PM: Networking Event", endTime.ToString());
             resultListBox.Items.Add(str.ToString());
         }
 
+        /**********************************************************
+         * Name: CoreDataAnalysis
+         * Function: Core analysis for conference schedule         
+         * Parameters： None
+         * Return: void         
+         **********************************************************/
         private void CoreDataAnalysis(TimeSpan startTime, TimeSpan endTime, List<string> dataSource)
         {
             var countOfTimeSpanDatas = 0;
@@ -131,7 +183,14 @@ namespace ConferenceScheduleTest
                 return;
             }
            
+            //
+            // Get the time span list count
+            //
             countOfTimeSpanDatas = (Int16)timeSpanDatas.Count;
+
+            //
+            // Get the time gap and turn to total minutes
+            //
             totalTimeMinutes = (Int16)endTime.Subtract(startTime).TotalMinutes;
             while (index < countOfTimeSpanDatas)
             {
@@ -154,6 +213,10 @@ namespace ConferenceScheduleTest
                         else
                         {
                             startTime = startTime.Add(new TimeSpan(0, timeSpanMinutes, 0));
+                            
+                            //
+                            // Judge the time belongs to AM or PM
+                            //
                             if (startTime.CompareTo(new TimeSpan(12, 0, 0)) > 0)
                             {
                                 str = string.Format("{0} PM: {1}", startTime.ToString(), dataSource[index]);
@@ -163,12 +226,14 @@ namespace ConferenceScheduleTest
                                 str = string.Format("{0} AM: {1}", startTime.ToString(), dataSource[index]);
                             }
                             resultListBox.Items.Add(str.ToString());
+
                             timeSpanDatas.RemoveAt(index);
                             if (timeSpanDatas.Count == 0)
                             {
                                 break;
                             }
                             countOfTimeSpanDatas--;
+
                             dataSource.RemoveAt(index);
                             if (dataSource.Count == 0)
                             {
@@ -197,12 +262,14 @@ namespace ConferenceScheduleTest
                         str = string.Format("{0} AM: {1}", startTime.ToString(), dataSource[index]);
                     }
                     resultListBox.Items.Add(str.ToString());
+
                     timeSpanDatas.RemoveAt(index);
                     if(timeSpanDatas.Count == 0)
                     {
                         break;
                     }
                     countOfTimeSpanDatas--;
+
                     dataSource.RemoveAt(index);
                     if (dataSource.Count == 0)
                     {
@@ -217,38 +284,52 @@ namespace ConferenceScheduleTest
             }
         }
 
-        //private void RecordInformation(TimeSpan startTime, Int16 timeSpan, Int16 currentIndex, List<string> dataSource)
-        //{
-        //    var str = "";
-        //    startTime = startTime.Add(new TimeSpan(0, timeSpan, 0));
-        //    if (startTime.CompareTo(new TimeSpan(12, 0, 0)) > 0)
-        //    {
-        //        str = string.Format("{0} PM: {1}", startTime.ToString(), dataSource[currentIndex]);
-        //    }
-        //    else
-        //    {
-        //        str = string.Format("{0} AM: {1}", startTime.ToString(), dataSource[currentIndex]);
-        //    }
-        //    resultListBox.Items.Add(str.ToString());
-        //    timeSpanDatas.RemoveAt(currentIndex);
-        //    //countOfTimeSpanDatas--;
-        //    dataSource.RemoveAt(currentIndex);
-        //    //timeSpanMinutes = tempTimeSpan;
-        //}
+        /******************************************
+         * Name: RecordInformation
+         * Function: Record the conference information
+         * Parameters： None
+         * Return: void
+         * ****************************************/
+        private void RecordInformation(TimeSpan startTime, Int16 timeSpan, Int16 currentIndex, List<string> dataSource)
+        {
+            var str = "";
+            startTime = startTime.Add(new TimeSpan(0, timeSpan, 0));
+            if (startTime.CompareTo(new TimeSpan(12, 0, 0)) > 0)
+            {
+                str = string.Format("{0} PM: {1}", startTime.ToString(), dataSource[currentIndex]);
+            }
+            else
+            {
+                str = string.Format("{0} AM: {1}", startTime.ToString(), dataSource[currentIndex]);
+            }
+            resultListBox.Items.Add(str.ToString());
+            timeSpanDatas.RemoveAt(currentIndex);           
+            dataSource.RemoveAt(currentIndex);            
+        }
 
+        /**********************************************************
+         * Name: DataAnalysisWithSort
+         * Function: Test the conference schedule with sorting         
+         * Parameters： None
+         * Return: void         
+         **********************************************************/
         private void DataAnalysisWithSort()
         {
+            // 
+            // Sort the original datas
+            //
             origDatas.Sort();
 
             var i = 0;
-
-            //clear the timeSpanDatas List
-            //for (; i < timeSpanDatas.Count; i++)
-            //{
-            //    timeSpanDatas.RemoveAt(i);
-            //}
+  
+            //
+            // Clear the time span data list
+            //
             timeSpanDatas.Clear();
 
+            //
+            // Re-construct the time span data according to original data after sorting
+            //
             for (; i< origDatas.Count; i++)
             {
                 string[] strSplit = origDatas[i].Split('/');
@@ -258,8 +339,6 @@ namespace ConferenceScheduleTest
                     return;
                 }
 
-
-
                 var index = strSplit[1].IndexOf("min");
                 timeSpanDatas.Add(Convert.ToInt16(strSplit[1].Substring(0, index)));
                 i++;
@@ -268,26 +347,37 @@ namespace ConferenceScheduleTest
             TimeSpan startTime = new TimeSpan(9, 0, 0);
             TimeSpan midTime = new TimeSpan(12, 0, 0);
             TimeSpan endTime = new TimeSpan(17, 0, 0);
-            //List<string> tempDatas = origDatas;
-
+          
+            //
             // Start information
+            //
             var str = string.Format("The second data with sorting:");
             resultListBox.Items.Add(str.ToString());
 
+            //
             // Morning schedule
+            //
             CoreDataAnalysis(startTime, midTime, origDatas);
 
-            // Midnoon for Lunch
+            //
+            // Middle noon for Lunch
+            //
             str = string.Format("{0} PM: Lunch", midTime.ToString());
             resultListBox.Items.Add(str.ToString());
 
+            //
             // After lunch, begin with 13 : 00
+            // 
             midTime = midTime.Add(new TimeSpan(1, 0, 0));
 
+            //
             // Afternoon schedule
+            //
             CoreDataAnalysis(midTime, endTime, origDatas);
 
+            //
             // End information
+            //
             str = string.Format("{0} PM: Networking Event", endTime.ToString());
             resultListBox.Items.Add(str.ToString());
         }
